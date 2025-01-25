8a78d9455f4b9d6ad7e6588e78e10f27210ca914
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2025-01-25 11:16:10 -0800

    Finished login with Zustand

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { createClient } from "@supabase/supabase-js";
+import { AccountStore } from "../../store/AccountStore";
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_API_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 function Dashboard() {
   const navigate = useNavigate();
 
+  // Zustand account store
+  const account_email = AccountStore((state) => state.account_email);
+
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
+
     if (!error) {
+      // Clear Zustand account store
+      setAccount({ account_name: "", account_email: "", account_uuid: "" });
+
       navigate("/login");
     } else {
       console.error("Error logging out:", error);
@@ -19,7 +27,7 @@ function Dashboard() {
   return (
     <div id="dashboard">
       <div className="row">
-        <div className="col-8">Hello</div>
+        <div className="col-8">Hello {account_email}</div>
         <div className="col-4 bg-danger-subtle">Hi</div>
       </div>
       <button onClick={handleLogout}>Logout</button>

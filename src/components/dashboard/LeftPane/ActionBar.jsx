1567e21c7556d75acf1fff1e19372f41707091d5
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2025-01-26 00:57:45 -0800

    Update ActionBar.jsx

--- src/components/dashboard/LeftPane/ActionBar.jsx
+++ src/components/dashboard/LeftPane/ActionBar.jsx
@@ -1,60 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { usePetData } from "../../../UserData";
 
-let count = 0;
-
-
-function printMoodWords() {
+export default function ActionBar() {
   const petData = usePetData();
-  
-  const encouragingWords = ["Good luck! You’re going to do great.",
-    "Break a leg! Go out there and wow them.", 
-    "You killed it! That was amazing.", 
+  const [count, setCount] = useState(0);
+
+  const encouragingWords = [
+    "Good luck! You’re going to do great.",
+    "Break a leg! Go out there and wow them.",
+    "You killed it! That was amazing.",
     "Congratulations, I’m so happy for you!",
     "You had the courage to follow your dreams.",
-    "You stuck to your guns, good for you!", "Shine on!", 
-    "One step at a time, you’re almost there!", 
-    "Don’t worry, you can do this!", "Keep at it, you’ve got this!", 
-    "I have no doubt that you’ll be great at this.", 
-    "This is tough, but you’re definitely tougher.", 
+    "You stuck to your guns, good for you!",
+    "Shine on!",
+    "One step at a time, you’re almost there!",
+    "Don’t worry, you can do this!",
+    "Keep at it, you’ve got this!",
+    "I have no doubt that you’ll be great at this.",
+    "This is tough, but you’re definitely tougher.",
     "It sucks that you have to go through this, but I know you can do it.",
-    "Things will get better, hang in there!", 
+    "Things will get better, hang in there!",
     "Whatever the universe has in store for you is going to be amazing.",
-    "Trust the process, it’ll work out.", 
+    "Trust the process, it’ll work out.",
     "Good vibes coming your way.",
     "Sending you big hugs and happy thoughts.",
     "You’re in my thoughts today.",
     "I’m here for you no matter what.",
     "Your friendship is important to me.",
     "You have a heart of gold.",
-    "You make the world a better place."]
+    "You make the world a better place.",
+  ];
 
+  const sadWords = [
+    "I feel blue.",
+    "I’m down in the dumps.",
+    "I have a heavy heart.",
+    "I’m feeling out of sorts.",
+    "I’m in a funk.",
+    "I’m heartbroken.",
+    "I’m downhearted.",
+    "I’m grieving.",
+    "I feel like I have a cloud hanging over me.",
+    "I’m at a low ebb.",
+    "I feel dispirited.",
+  ];
 
-  const sadWords = ["I feel blue.", "I’m down in the dumps.", 
-    "I have a heavy heart.", "I’m feeling out of sorts.", "I’m in a funk.",
-    "I’m heartbroken.", "I’m downhearted.", "I’m grieving.", 
-    "I feel like I have a cloud hanging over me.", "I’m at a low ebb.",
-    "I feel dispirited."]
+  const handleChatClick = () => {
+    // Increment count and wrap around the respective words array
+    setCount((prevCount) => prevCount + 1);
 
-  if (petData?.mood > 1) {
-    count += 1
-    if (count > 24){
-      count = 0
-    }
-    console.log(encouragingWords[count])
-      return encouragingWords[count]
-  }
-  else {
-    count += 1
-    if (count > 10) {
-      count = 0
-    }
-      return sadWords[count]
-  }
+    const words =
+      petData?.mood > 2 ? encouragingWords : sadWords;
 
-}
-export default function ActionBar() {
-  
+    // Compute the current message based on the updated count
+    const message = words[count % words.length];
+    console.log(message);
+    return message;
+  };
+
   return (
     <div id="action-bar" className="rounded px-5">
       <div
@@ -79,12 +82,11 @@ export default function ActionBar() {
         <button
           type="button"
           className="action-btn btn btn-outline-secondary px-5 py-4"
-          onclick={() => console.log(printMoodWords())}
+          onClick={handleChatClick}
         >
           <span className="fs-5">💬</span> <br />
           Chat
         </button>
-        
       </div>
     </div>
   );
